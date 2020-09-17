d686bdb3ba56354bc9593044ab86878b68951d5b
jjchen98 <70703602+jjchen98@users.noreply.github.com>
2020-09-18 00:01:32 +0800

    Update anomalies.py

--- anomalies.py
+++ anomalies.py
@@ -1,1 +1,25 @@
 
+import pandas
+from pathlib import Path
+import argparse
+import json
+from datetime import datetime
+import typing as T
+import matplotlib.pyplot as plt
+import numpy as np
+
+good_temp= []
+bad_temp = []
+filer = open('dataset.txt')
+data = load_data('dataset.txt')
+onestd = statistics.pstdev(data)
+
+
+for k in data:
+  if ((k - staticstics.mean(data))>=onestd*3):
+    bad_temp.append(k)
+  else:
+    good_temp.append(k)
+    
+    
+ 
